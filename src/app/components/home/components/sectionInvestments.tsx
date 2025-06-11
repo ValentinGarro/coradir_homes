d1cc2e6d37d410b82bb7cb223df9697b749d04e6
ValentinGarro <vgarro@coradir.com.ar>
2025-06-11 11:59:43 -0300

    arreglando estilos de home desktop

--- src/app/components/home/components/sectionInvestments.tsx
+++ src/app/components/home/components/sectionInvestments.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function SectionInvestments(){
     return(
         <section className="w-full xl:w-[80%] flex flex-col xl:flex-row items-center justify-center gap-10">
-            <h2 className="font-playfair text-black text-4xl xl:text-6xl">
+            <h2 className="font-raleway text-black text-4xl xl:text-6xl">
                 Tu oportunidad de <br/> 
                 <b className="text-blue font-bold font-playfair text-5xl xl:text-9xl">Inversión</b>
             </h2>
-            <div className="w-full flex flex-col justify-center items-center gap-5  ">
+            <div className="w-full flex flex-col justify-center items-center gap-5 xl:!w-[30%] ">
                 <p className="text-lg xl:text-xl font-raleway text-black">
                     <b>Invertí con seguridad,</b> incluso sin experiencia previa,
                     y sé parte activa del desarrollo inmobiliario.
